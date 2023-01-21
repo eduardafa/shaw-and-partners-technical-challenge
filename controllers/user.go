705c74e65be84e62b9ddb28e718396a14aa79280
Eduarda França <eduardafandrade@gmail.com>
2023-01-20 21:20:36 -0330

    feat: controllers for user requisition implemented

--- controllers/user.go
+++ controllers/user.go
@@ -1,55 +1,68 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
-	"io/ioutil"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"shaw-and-partners-technical-challenge/domain"
+	"shaw-and-partners-technical-challenge/handler"
 )
 
-var ApiUrl = os.Getenv("GITHUB_API_URL")
-
-func GetUserDetails(_ http.ResponseWriter, r *http.Request) {
+func GetUserDetails(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	username := vars["username"]
 
-	url := fmt.Sprintf(ApiUrl+"/users/%s", username)
-	//response, err := http.Get(ApiUrl + "/users/" + username)
-	response, err := http.Get(url)
-	fmt.Println("response>>>>>>>", response)
-	if err != nil {
-		fmt.Print(err.Error())
-		//os.Exit(1)
+	url := domain.GlobalConfig.GithubApiUrl + "/users/" + username
+	responseUrl, errGetReq := http.Get(url)
+	if errGetReq != nil {
+		fmt.Print(errGetReq.Error())
+		os.Exit(1)
 	}
-	responseData, err := ioutil.ReadAll(response.Body)
+	responseData, err := io.ReadAll(responseUrl.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(responseData))
+
+	var responseObject domain.User
+	errUnmarshal := json.Unmarshal(responseData, &responseObject)
+	if errUnmarshal != nil {
+		log.Fatal(errUnmarshal)
+	}
+	response := handler.NewJsonResponse(responseObject, nil, handler.MetaResponse{
+		Count: 1,
+	})
+	handler.SendJsonResponse(w, response, http.StatusOK)
 }
 
-//func GetPaintCansByRoom(w http.ResponseWriter, r *http.Request) {
-//	roomModel := models.Room{}
-//	decoder := json.NewDecoder(r.Body)
-//	decoder.DisallowUnknownFields()
-//	errDecoder := decoder.Decode(&roomModel)
-//	if errDecoder != nil {
-//		handler.SendErrorResponse(w, &handler.ApiError{
-//			Detail: errDecoder.Error(),
-//			Status: http.StatusBadRequest,
-//		})
-//		return
-//	}
-//
-//	errCalculate, paintCansQuantities := models.CalculateAreaToBePaintedByRoom(roomModel)
-//	if errCalculate != nil {
-//		handler.SendErrorResponse(w, errCalculate)
-//		return
-//	}
-//	response := handler.NewJsonResponse(paintCansQuantities, nil, handler.MetaResponse{
-//		Count: 1,
-//	})
-//	handler.SendJsonResponse(w, response, http.StatusOK)
-//}
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	number := query.Get("since")
+
+	url := domain.GlobalConfig.GithubApiUrl + "/users?since=" + number
+	responseUrl, errGetReq := http.Get(url)
+	if errGetReq != nil {
+		fmt.Print(errGetReq.Error())
+		os.Exit(1)
+	}
+	link := responseUrl.Header.Get("link")
+
+	responseData, err := io.ReadAll(responseUrl.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseObject []domain.SummarizedUserData
+	errUnmarshal := json.Unmarshal(responseData, &responseObject)
+	if errUnmarshal != nil {
+		log.Fatal(errUnmarshal)
+	}
+	response := handler.NewJsonResponse(responseObject, nil, handler.MetaResponseWithPagination{
+		Count: len(responseObject),
+		Link:  link,
+	})
+	handler.SendJsonResponse(w, response, http.StatusOK)
+}
