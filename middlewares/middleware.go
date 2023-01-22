7aff8c2716791e7f57112cd727403d2f2c22c30c
Eduarda França <eduardafandrade@gmail.com>
2023-01-22 01:32:40 -0330

    refactor: method name changed

--- middlewares/middleware.go
+++ middlewares/middleware.go
@@ -1,8 +1,8 @@
-package middleware
+package middlewares
 
 import "net/http"
 
-func SetContentTypeMiddleware(next http.Handler) http.Handler {
+func SetAcceptMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Accept", "application/vnd.github+json")
 		next.ServeHTTP(w, r)
